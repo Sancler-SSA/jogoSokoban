f7f0b34bd086186452bf7fb7ba55be6714c02fe6
Sancler <sanclerprodigy@gmail.com>
2024-09-16 17:33:01 -0300

    06-09-2024

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const board2 = document.getElementById('board2');
-
+const M=8;
+const N=8;
+for(let k=0; k<M; k++) {
 const row = document.createElement('div');
 row.classList.add('row');
 
-
-for(let i=0; i<4;i++) {
+for(let i=0; i<N;i++) {
 const celula = document.createElement('div');
 celula.classList.add('cell');
-
 row.append(celula);
 
+// if(i == 0 || i == N - 1 || k == 0 || k == M - 1){
+//     celula.classList.add('vazio');
+// }
+
+if(i > 0 && i < 7 && k > 0 && k < 7) {
+    celula.classList.add('vazio');
+}
+}
+board2.append(row);
 }
 
-board2.append(row);
