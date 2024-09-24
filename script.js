6474204317376c32907ce28854ff78dace308008
Sancler <sanclerprodigy@gmail.com>
2024-09-24 17:26:16 -0300

    24-09-2024 - segunda

--- script.js
+++ script.js
@@ -1,72 +1,78 @@
-function rule0(celula, k, i){ 
-    if (k <= 4) {
-        celula.classList.add('empty');
-    }
-}
-    builGameBoard(8, 8, rule0);
-
-
-function builGameBoard(numberOfRows, numberOfcollumns, drowrule) {
-    const game = document.getElementById("game");
-    const board = document.createElement('div');
-    board.classList.add('tabuleiro');
-
-
-    for (let k = 0; k < numberOfRows; k++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        board.append(row);
-
-        for (let i = 0; i <numberOfcollumns; i++) {
-            const celula = document.createElement('div');
-            celula.classList.add('cell');
-            row.append(celula);
-
-            drowrule(celula, numberOfRows, numberOfcollumns, k, i);
-        }
-    }
-    game.append(board);
-
-
-
-
-
-const player = new Player(0, 0);
-const celulas = document.querySelectorAll('.cell');
-const playerElement = document.querySelector('.player');
-
-
 const distanciaSalto = 66;
 const margirFix = 4;
+const NUM_ROWS = 8;
+const NUM_COLS = 8;
+
+builGameBoard(NUM_ROWS, NUM_COLS);
+
+const player = new Player(0, 0);
+const playerElement = document.querySelector('.player');
 
 playerElement.style.top = calculaPosicao(0);
 playerElement.style.left = calculaPosicao(0);
 
-
-
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
-    
+
+    if (verifyPosition(next)) {
+        player.moveTo(next, playerElement);
+    }
 })
 
+function Player(x, y) {
+    this.x = x;
+    this.y = y;
+    this.nextPosition = function (keycode) {
+        let { x, y } = player;
+        if (keycode === "ArrowUp") x--;
+        if (keycode === "ArrowDown") x++;
+        if (keycode === "ArrowLeft") y--;
+        if (keycode === "ArrowRight") y++;
+        return { x, y };
+    }
+    this.moveTo = function (position, element) {
+        let { x, y } = position;
+        this.x = position.x;
+        this.y = position.y;
+
+        element.style.top = calculaPosicao(this.x);
+        element.style.left = calculaPosicao(this.y);
+    }
 }
+
 function verifyPosition(position) {
     console.log(position);
     let { x, y } = position;
-    return x >= 0 && x < 4 && y >= 0 && y < 4;
+    return x >= 0 && x < NUM_ROWS && y >= 0 && y < NUM_COLS;
 }
 
 function calculaPosicao(qtd) {
-   
+
     return `${qtd * distanciaSalto + margirFix}px`;
 }
 
-// // console.log(calculaPosicao(0, 64) === "0px");
-// // console.log(calculaPosicao(1, 64) === "64px");
-// // console.log(calculaPosicao(2, 32) === "64px");
-// // console.log(calculaPosicao(10, 60) === "600px");
-// // console.log(calculaPosicao(-3, 45) === "-135px");
+function createGameElement(elementName, className, parentNode) {
+    const element = document.createElement(elementName);
+    element.classList.add(className);
+    parentNode.append(element);
+    return element;
+}
 
+function builGameBoard(numberOfRows, numberOfcollumns) {
+    const game = document.getElementById("game");
+    const board = createGameElement('div', 'tabuleiro', game);
+   
+    
+    for (let k = 0; k < numberOfRows; k++) {
+        const row = createGameElement('div', 'row', board);
+        board.append(row);
 
+        for (let i = 0; i < numberOfcollumns; i++) {
+            const celula = createGameElement('div', 'cell', row);
+            row.append(celula);
+        }
+    }
+    createGameElement('div', 'player', board);
 
+}
 
