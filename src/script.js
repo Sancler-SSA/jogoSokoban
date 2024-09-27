f25bd237148ca1895d12d9c571c7be7be679a3d9
Sancler <sanclerprodigy@gmail.com>
2024-09-27 16:43:58 -0300

    refatora o metodo piece.prototype.move

--- src/script.js
+++ src/script.js
@@ -4,65 +4,33 @@ const margirFix = 4;
 const pieces = builGameBoard(NUM_ROWS, NUM_COLS);
 const board = document.querySelector('.tabuleiro');
 
-const player = new Piece(pieces.player.x, pieces.player.y);
-const playerElement =  createGameElement('div', 'player', board);
+const player = createBoardPiece(pieces.player, 'player');
 
+function createBoardPiece(piecePosition, className) {
+    const piece = new Piece(piecePosition.x, piecePosition.y);
+    piece.insertElementInto(className, board);
 
-
-function createBoardPiece(piece, className) {
-    piece.insertElementInfo(className, board);
-
-// const element =  createGameElement('div', className, board);
-
-// element.style.top = calculaPosicao(piece.x);
-// element.style.left = calculaPosicao(piece.y);
-
-return piece.element;
-
-
+    return piece;
 }
 
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
 
     if (verifyPosition(next)) {
-        player.moveTo(next, playerElement);
+        player.moveTo(next);
     }
 })
+
+function verifyPosition(position) {
+    console.log(position);
+    let { x, y } = position;
+    return boardMap[x][y] !== '#';
+}
+
 
 function calculaPosicao(qtd) {
 
     return `${qtd * distanciaSalto + margirFix}px`;
 }
 
-
-function builGameBoard(numberOfRows, numberOfcollumns) {
-    const pieces = {};
-
-    const game = document.getElementById("game");
-    const board = createGameElement('div', 'tabuleiro', game);
-  
-
-    for (let i = 0; i < numberOfRows; i++) {
-        const row = createGameElement('div', 'row', board);
-        board.append(row);
-
-        for (let j = 0; j < numberOfcollumns; j++) {
-            const cell = createGameElement('div', 'cell', row);
-
-            const char = boardMap[i][j];
-
-
-            if (char === '#') cell.classList.add(['wall']);
-            if (char === 'G') cell.classList.add('goal');
-            if (char === 'B') cell.classList.add('box');
-            if (char === 'P') pieces.player = { x: i, y: j };
-
-        }
-
-    }
-   
-    return pieces;
-
-}
 
