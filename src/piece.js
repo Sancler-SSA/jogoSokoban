41ac0a5a06e4cb0f8d65032e39faabb30251e72f
Sancler <sanclerprodigy@gmail.com>
2024-09-27 17:26:35 -0300

    correção

--- src/piece.js
+++ src/piece.js
@@ -17,16 +17,20 @@ function Piece(x, y) {
         this.x = position.x;
         this.y = position.y;
 
-        this.element.style.top = calculaPosicao(this.x);
-        this.element.style.left = calculaPosicao(this.y);
+        this.updateElementPosition();
     }
 
     this.insertElementInto = function(className, parent){
         this.element =  createGameElement('div', className, parent);
 
+        this.updateElementPosition();
+        
+    }
+    
+    this.updateElementPosition = function () {
+        
         this.element.style.top = calculaPosicao(this.x);
         this.element.style.left = calculaPosicao(this.y);
-
     }
 
 }
